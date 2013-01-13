2d116bac5a2eadf8a51a87b0c3921fff8d2370dc
SpaceEmotion <spaceemotion@gmail.com>
2013-01-13 18:49:18 +0100

    First half-way working version

--- src/main/java/net/catharos/recipes/RecipeLoader.java
+++ src/main/java/net/catharos/recipes/RecipeLoader.java
@@ -1,8 +1,10 @@
 package net.catharos.recipes;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
+import net.catharos.recipes.crafting.CustomRecipe;
 import net.catharos.recipes.crafting.CustomShapedRecipe;
 import net.catharos.recipes.crafting.CustomShapelessRecipe;
 
@@ -10,16 +12,16 @@ import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class RecipeLoader {
-	private enum RecipeType {
-		SHAPED, SHAPELESS
-	}
+	protected cRecipes plugin;
 
-	public RecipeLoader( JavaPlugin plugin ) {
+	public RecipeLoader( cRecipes plugin ) {
+		this.plugin = plugin;
+
 		File recFile = new File( plugin.getDataFolder(), "recipes.yml" );
 		if (!recFile.exists()) return;
 
@@ -28,16 +30,16 @@ public class RecipeLoader {
 		// Shaped recipes
 		ConfigurationSection shaped = recipes.getConfigurationSection( "shaped" );
 		for (String key : shaped.getKeys( false ))
-			if (shaped.isConfigurationSection( key )) this.loadRecipe( key, shaped.getConfigurationSection( key ), RecipeType.SHAPED );
+			if (shaped.isConfigurationSection( key )) this.loadRecipe( key, shaped.getConfigurationSection( key ), true );
 
 		// Shapeless recipes
 		ConfigurationSection shapeless = recipes.getConfigurationSection( "shapeless" );
 		for (String key : shapeless.getKeys( false ))
-			if (shapeless.isConfigurationSection( key )) this.loadRecipe( key, shapeless.getConfigurationSection( key ), RecipeType.SHAPED );
+			if (shapeless.isConfigurationSection( key )) this.loadRecipe( key, shapeless.getConfigurationSection( key ), false );
 
 	}
 
-	public boolean loadRecipe( String name, ConfigurationSection config, RecipeType type ) {
+	public boolean loadRecipe( String name, ConfigurationSection config, boolean shaped ) {
 		// Output material
 		String[] out = config.getString( "block" ).split( ":" );
 
@@ -49,12 +51,34 @@ public class RecipeLoader {
 		int amount = config.getInt( "amount", 1 );
 
 		// Add the recipe
-		if (type == RecipeType.SHAPED)
-			return addShaped( new CustomShapedRecipe( name, mat, amount, data ), config );
-		else if (type == RecipeType.SHAPELESS)
-			return addShapeless( new CustomShapelessRecipe( name, mat, amount, data ), config );
-		else
-			return false;
+		CustomRecipe cr;
+
+		if (shaped) {
+			cr = new CustomShapedRecipe( name, mat, amount, data );
+			if (!addShaped( (CustomShapedRecipe) cr, config )) return false;
+		} else {
+			cr = new CustomShapelessRecipe( name, mat, amount, data );
+			if (!addShapeless( (CustomShapelessRecipe) cr, config )) return false;
+		}
+
+		// Item drops
+		List<ItemStack> drops = new ArrayList<ItemStack>();
+
+		if (config.isList( "drops" )) {
+			for (String s : config.getStringList( "drops" )) {
+				String[] l = s.split( ":" );
+
+				if (l.length == 2)
+					drops.add( new ItemStack( Material.getMaterial( l[0] ), 1, Short.parseShort( l[1] ) ) );
+				else
+					drops.add( new ItemStack( Material.getMaterial( l[0] ), 1 ) );
+			}
+		} else {
+			drops.add( cr.getItem() );
+		}
+
+		cr.setDrops( drops );
+		return true;
 	}
 
 	private boolean addShaped( CustomShapedRecipe cr, ConfigurationSection c ) {
@@ -62,19 +86,20 @@ public class RecipeLoader {
 		ConfigurationSection rc = c.getConfigurationSection( "recipe" );
 
 		if (rc != null) {
-			ConfigurationSection ingredients = rc.getConfigurationSection( "ingedients" );
+			ConfigurationSection ingredients = rc.getConfigurationSection( "ingredients" );
 			for (String key : ingredients.getKeys( false )) {
 				String[] i = ingredients.getString( key ).split( ":" );
 
-				if (i.length > 1) {
+				if (i.length > 1)
 					recipe.setIngredient( key.charAt( 0 ), Material.getMaterial( Integer.parseInt( i[0] ) ), Integer.parseInt( i[1] ) );
-				} else if (i.length == 1) {
+				else
 					recipe.setIngredient( key.charAt( 0 ), Material.getMaterial( Integer.parseInt( i[0] ) ) );
-				}
 			}
 
 			List<String> shapeList = rc.getStringList( "shape" );
 			recipe.shape( shapeList.toArray( new String[shapeList.size()] ) );
+
+			plugin.addRecipe( cr.getItem().getTypeId(), cr.getItem().getData().getData(), cr );
 
 			return true;
 		}
@@ -90,12 +115,13 @@ public class RecipeLoader {
 			for (String key : rc.getKeys( false )) {
 				String[] i = rc.getString( key ).split( ":" );
 
-				if (i.length > 1) {
+				if (i.length > 1)
 					recipe.addIngredient( key.charAt( 0 ), Material.getMaterial( Integer.parseInt( i[0] ) ), Integer.parseInt( i[1] ) );
-				} else if (i.length == 1) {
+				else
 					recipe.addIngredient( key.charAt( 0 ), Material.getMaterial( Integer.parseInt( i[0] ) ) );
-				}
 			}
+
+			plugin.addRecipe( cr.getItem().getTypeId(), cr.getItem().getData().getData(), cr );
 
 			return true;
 		}
--- src/main/java/net/catharos/recipes/cRecipes.java
+++ src/main/java/net/catharos/recipes/cRecipes.java
@@ -1,19 +1,69 @@
 package net.catharos.recipes;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import net.catharos.recipes.crafting.CustomRecipe;
+
 import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class cRecipes extends JavaPlugin implements Listener {
+	protected Map<Integer, Map<Byte, CustomRecipe>> recipes;
 
 	public void onEnable() {
+		getDataFolder().mkdirs();
+
+		recipes = new HashMap<Integer, Map<Byte, CustomRecipe>>();
+
 		getServer().getPluginManager().registerEvents( this, this );
+	}
+
+	/**
+	 * Returns a list of stored {@link CustomRecipe}s
+	 * 
+	 * @return A list of {@link CustomRecipe}s
+	 */
+	public Map<Integer, Map<Byte, CustomRecipe>> getRecipes() {
+		return recipes;
+	}
+
+	public void addRecipe( int mat, byte data, CustomRecipe recipe ) {
+		Map<Byte, CustomRecipe> map = getRecipes().get( mat );
+
+		if (map == null) {
+			map = new HashMap<Byte, CustomRecipe>();
+			recipes.put( mat, map );
+		}
+
+		map.put( data, recipe );
 	}
 
 	@EventHandler
 	public void e( BlockBreakEvent event ) {
 		if (event.getPlayer().getGameMode() == GameMode.CREATIVE) return;
+
+		Block block = event.getBlock();
+
+		for (Entry<Integer, Map<Byte, CustomRecipe>> e : getRecipes().entrySet()) {
+			if (e.getKey() != block.getTypeId()) continue;
+
+			CustomRecipe cr = e.getValue().get( block.getData() );
+
+			if (cr != null) {
+				for (ItemStack drop : cr.getDrops())
+					block.getWorld().dropItemNaturally( block.getLocation(), drop );
+
+				block.setType( Material.AIR );
+				event.setCancelled( true );
+			}
+		}
 	}
 }
